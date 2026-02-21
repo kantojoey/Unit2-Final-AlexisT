a7ff2248d771da440b4acf2cddde269f80235913
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-02-21 13:07:10 -0600

    Created spring boot application

---
+++ hit-record-backend/src/main/java/com/example/hit_record_backend/HitRecordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hit_record_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitRecordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitRecordBackendApplication.class, args);
+	}
+
+}
---
+++ hit-record-backend/src/test/java/com/example/hit_record_backend/HitRecordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hit_record_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitRecordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
