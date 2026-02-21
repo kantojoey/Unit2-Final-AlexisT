b4a01606d30ffc649ff3c7c4a7fb67c2b85c85b5
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-02-21 15:44:53 -0600

    Created controllers and started initial UserController build

---
+++ hit-record-backend/src/main/java/com/example/hit_record_backend/controllers/UserController.java
@@ -0,0 +1,40 @@
+package com.example.hit_record_backend.controllers;
+
+
+import com.example.hit_record_backend.models.User;
+import com.example.hit_record_backend.repositories.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
