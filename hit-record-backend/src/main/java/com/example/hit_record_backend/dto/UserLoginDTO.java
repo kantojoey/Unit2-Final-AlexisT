634448fcb8cbcae5f6615a5dfeb4f75c63ab257f
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-02-26 18:45:07 -0600

    Added constructors to DTOs

--- hit-record-backend/src/main/java/com/example/hit_record_backend/dto/UserLoginDTO.java
+++ hit-record-backend/src/main/java/com/example/hit_record_backend/dto/UserLoginDTO.java
@@ -4,6 +4,11 @@ public class UserLoginDTO {
     private String username;
     private String password;
 
+    public UserLoginDTO(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
     }
