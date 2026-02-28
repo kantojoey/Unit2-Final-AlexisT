33d330bcc24319f37a843bd2c44f4d300df676c2
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-02-28 01:10:29 -0600

    Built AlbumResponseDTO

--- hit-record-backend/src/main/java/com/example/hit_record_backend/dto/response/AlbumResponseDTO.java
+++ hit-record-backend/src/main/java/com/example/hit_record_backend/dto/response/AlbumResponseDTO.java
@@ -1,4 +1,61 @@
 package com.example.hit_record_backend.dto.response;
 
 public class AlbumResponseDTO {
+
+    private String title;
+    private String artist;
+    private Integer yearReleased;
+    private Integer numberOfTracks;
+    private String spotifyAlbumId;
+
+    public AlbumResponseDTO(String title, String artist, Integer yearReleased, Integer numberOfTracks, String spotifyAlbumId) {
+        this.title = title;
+        this.artist = artist;
+        this.yearReleased = yearReleased;
+        this.numberOfTracks = numberOfTracks;
+        this.spotifyAlbumId = spotifyAlbumId;
+    }
+
+    public AlbumResponseDTO(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Integer getYearReleased() {
+        return yearReleased;
+    }
+
+    public void setYearReleased(Integer yearReleased) {
+        this.yearReleased = yearReleased;
+    }
+
+    public Integer getNumberOfTracks() {
+        return numberOfTracks;
+    }
+
+    public void setNumberOfTracks(Integer numberOfTracks) {
+        this.numberOfTracks = numberOfTracks;
+    }
+
+    public String getSpotifyAlbumId() {
+        return spotifyAlbumId;
+    }
+
+    public void setSpotifyAlbumId(String spotifyAlbumId) {
+        this.spotifyAlbumId = spotifyAlbumId;
+    }
 }
+
