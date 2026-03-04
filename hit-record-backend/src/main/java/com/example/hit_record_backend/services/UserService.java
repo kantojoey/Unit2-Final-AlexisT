502fa97d2472be9ad68a54b6caf5f75d6ff76bc7
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-03-04 00:47:34 -0600

    Added missing annotation to UserService

--- hit-record-backend/src/main/java/com/example/hit_record_backend/services/UserService.java
+++ hit-record-backend/src/main/java/com/example/hit_record_backend/services/UserService.java
@@ -7,12 +7,14 @@ import com.example.hit_record_backend.models.User;
 import com.example.hit_record_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
 public class UserService {
 
     @Autowired
