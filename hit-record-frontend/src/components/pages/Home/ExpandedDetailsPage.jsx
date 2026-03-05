9d616ab02af89d28d79511042a083e53c34d739a
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-03-04 20:17:43 -0600

    Refactored year to yearReleased to match backend

--- hit-record-frontend/src/components/pages/Home/ExpandedDetailsPage.jsx
+++ hit-record-frontend/src/components/pages/Home/ExpandedDetailsPage.jsx
@@ -31,7 +31,7 @@ const ExpandedDetailsPage = ({ expandedAlbum }) => {
                                 <h3 className="data-heading"><span className="data-category">Artist:</span> {expandedAlbum.artist}</h3>
                             </Card>
                             <Card className="album-datapoint-card ">
-                                <h3 className="data-heading"><span className="data-category">Year:</span> {expandedAlbum.year}</h3>
+                                <h3 className="data-heading"><span className="data-category">Year:</span> {expandedAlbum.yearReleased}</h3>
                             </Card>
                             <Card className="album-datapoint-card ">
                                 <h3 className="data-heading">{expandedAlbum.tracks} <span className="data-category">tracks</span></h3>
