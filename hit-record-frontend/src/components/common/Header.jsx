81259660c883834a63c3a74e99ea4f2f79593c49
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-03-08 18:42:21 -0500

    Added log out button and adjusted styling

--- hit-record-frontend/src/components/common/Header.jsx
+++ hit-record-frontend/src/components/common/Header.jsx
@@ -1,12 +1,17 @@
 import { BsVinylFill } from "react-icons/bs";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const Header = () => {
     return (
         <>
             <header>
                 <div className="header-content">
-                    <BsVinylFill className="record-icon"/>
-                    <h1 className = "header-text">Hit Record</h1>
+                    <BsVinylFill className="record-icon" />
+                    <h1 className="header-text">Hit Record</h1>
+                </div>
+                <div className="logout-section">
+                    <FaSignOutAlt className="sign-out-icon" />
+                    <p>Log out</p>
                 </div>
             </header>
 
